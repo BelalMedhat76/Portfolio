272a3900c18a80aa61f83f59c042189c84196a1b
BelalMedhat76 <belalmedhat0102@gmail.com>
2024-11-29 10:19:49 +0200

    adding upload link

--- next.config.js
+++ next.config.js
@@ -6,3 +6,23 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+// next.config.js
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, ''); // Get repository name
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+module.exports = {
+  output: 'export',
+  assetPrefix,
+  basePath,
+  images: {
+    unoptimized: true, // Disable automatic image optimization for GitHub Pages
+  },
+};
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 import Button from "@/components/Button/Button";
 
 export const metadata = {
-  title: "Lama Dev Contact Information",
+  title: "countZero Contact Information",
   description: "This is Contact Page",
 };
 
 const Contact = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <h1 className={styles.title}>{`Let's Keep in Touch`}</h1>
       <div className={styles.content}>
         <div className={styles.imgContainer}>
           <Image
@@ -28,13 +28,11 @@ const Contact = () => {
             className={styles.textArea}
             placeholder="message"
             cols="30"
-            rows="10"
-          ></textarea>
-          <Button url="#" text="Send"/>
+            rows="10"></textarea>
+          <Button url="#" text="Send" />
         </form>
       </div>
     </div>
   );
 };
-
 export default Contact;
